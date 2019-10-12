da36f681f83ae37e6e6ae1722626d579a9488538
Vlad <vlad.petrov.phd@gmail.com>
2019-10-12 15:55:00 +0200

    cleanup

--- Domain/Project/ProjectDomain.cs
+++ Domain/Project/ProjectDomain.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Project
 {
     public class ProjectDomain : DomainBase
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Acronym { get; set; }
 
+        [Required]
         public DateTime StartDate { get; set; }
 
+        [Required]
         public DateTime EndDate { get; set; }
 
+        [Range(1, long.MaxValue)]
         public long Budget { get; set; }
     }
 }
