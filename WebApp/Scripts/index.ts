f7997493ab209f8a1b9157a99ad119bf449639fc
Vlad <vlad.petrov.phd@gmail.com>
2019-10-12 13:23:33 +0200

    cleanup

--- WebApp/Scripts/index.ts
+++ WebApp/Scripts/index.ts
@@ -5,7 +5,7 @@ export * from "./EndBalanceService/EndBalanceService";
 export * from "./manyToManyRelationService/manyToManyRelationService";
 export * from "./modalService/modalService";
 export * from "./tableService/tableService";
-//export * from "./FilesPreviewService/filesPreviewService";
+export * from "./FilesPreviewService/filesPreviewService";
 export * from "./EditNoteService/editNoteService";
 export * from "./EditProjectAndCategoryService/editProjectAndCategoryService";
 export * from "./FormScripts/formScripts";
