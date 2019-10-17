9da4017c894ff1c8ebbacb446282368757da0455
Vlad <vlad.petrov.phd@gmail.com>
2019-10-17 19:45:25 +0200

    unmask on submit

--- WebApp/Areas/BackOffice/Views/Clients/ClientFormDescriptor.cs
+++ WebApp/Areas/BackOffice/Views/Clients/ClientFormDescriptor.cs
@@ -8,12 +8,12 @@ namespace WebApp.Areas.BackOffice.Views.Clients
         public static FormDescriptor Get(bool readOnly)
         {
             return FormBuilder<ClientDomain>.CreateNew(readOnly, null)
-                //.AddItem(x => x.ClientName)
-                //.AddItem(x => x.PhoneNumber)
-                //.AddItem(x => x.Email)
+                .AddItem(x => x.ClientName)
+                .AddItem(x => x.PhoneNumber)
+                .AddItem(x => x.Email)
                 .AddItem(x => x.Balance)
-                //.AddItem(x => x.ContractStartDate)
-                //.AddItem(x => x.ContractEndDate)
+                .AddItem(x => x.ContractStartDate)
+                .AddItem(x => x.ContractEndDate)
                 .Build();
         }
     }
