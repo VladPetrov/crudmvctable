bdb7815ab203204f33751d03b9e42c86e460d3e3
Vlad <vlad.petrov.phd@gmail.com>
2019-10-18 14:56:07 +0200

    client firm

--- WebApp/Areas/BackOffice/Controllers/ClientFirmsController.cs
+++ WebApp/Areas/BackOffice/Controllers/ClientFirmsController.cs
@@ -1,9 +1,12 @@
 ﻿using BLL.Infrastructure;
+using Common.StringConstants;
 using Domain.Client;
+using Microsoft.AspNetCore.Mvc;
 using WebApp.Controllers;
 
 namespace WebApp.Areas.BackOffice.Controllers
 {
+    [Area(AreasNames.BackOfficeArea)]
     public class ClientFirmsController : ChildPageCrudController<ClientFirmDisplay, ClientFirmDomain, string>
     {
         public ClientFirmsController(IClientFirmService service) : base(service)
---
+++ WebApp/Areas/BackOffice/Views/ClientFirms/ClientFirmFormDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Client;
+using WebApp.Model.Forms;
+
+namespace WebApp.Areas.BackOffice.Views.ClientFirms
+{
+    public static class ClientFirmFormDescriptor
+    {
+        public static FormDescriptor Get(bool readOnly)
+        {
+            return FormBuilder<ClientFirmDomain>.CreateNew(readOnly, null)
+                .AddItem(x => x.Name)
+                .AddItem(x => x.Enabled)
+                .Build();
+        }
+    }
+}
