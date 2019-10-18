900d47f7947fdda8000fe0ec300fc0c9d8b7766f
Vlad <vlad.petrov.phd@gmail.com>
2019-10-18 15:22:49 +0200

    firms controller fix

--- WebApp/Areas/BackOffice/Controllers/ClientFirmsController.cs
+++ WebApp/Areas/BackOffice/Controllers/ClientFirmsController.cs
@@ -3,10 +3,12 @@ using Common.StringConstants;
 using Domain.Client;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Controllers;
+using WebApp.Model.GenericMvc;
 
 namespace WebApp.Areas.BackOffice.Controllers
 {
     [Area(AreasNames.BackOfficeArea)]
+    [SetAvailableTableActions(TableActions.Add, TableActions.Details, TableActions.Edit)]
     public class ClientFirmsController : ChildPageCrudController<ClientFirmDisplay, ClientFirmDomain, string>
     {
         public ClientFirmsController(IClientFirmService service) : base(service)
