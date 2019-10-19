a1b422b66422026ed8eb25e002ba65cd153924ba
Vlad <vlad.petrov.phd@gmail.com>
2019-10-19 12:12:59 +0200

    profile settings

---
+++ WebApp/Model/ProfileSettings/ProfileSettingsModel.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApp.Model.ProfileSettings
+{
+    public class ProfileSettingsModel
+    {
+        private ProfileSettingsModel()
+        {
+            
+        }
+
+        public bool BackOfficeArea { get; private set; }
+
+        public string MusterEntityFk { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+
+        public bool HasMusterEntityFk => MusterEntityFk != null;
+
+
+        public static ProfileSettingsModel CreateForBackOfficeArea(string musterEntityFk, bool isReadOnly = true)
+        {
+            return new ProfileSettingsModel
+            {
+                BackOfficeArea = true, 
+                MusterEntityFk = musterEntityFk,
+                IsReadOnly = isReadOnly
+            };
+        }
+
+        public static ProfileSettingsModel CreateForClientArea(string musterEntityFk)
+        {
+            return new ProfileSettingsModel { BackOfficeArea = false, MusterEntityFk = musterEntityFk };
+        }
+    }
+}
