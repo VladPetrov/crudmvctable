5bc2333489778d7130ad0b7a265e9883def090c5
Vlad <vlad.petrov.phd@gmail.com>
2019-10-21 12:27:47 +0200

    ajax content on click

---
+++ WebApp/Model/AjaxContentOnClickViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Model
+{
+    public class AjaxContentOnClickViewModel
+    {
+        public string Url { get; }
+        public string ClickObjectId { get; }
+
+        public AjaxContentOnClickViewModel(string url, string clickObjectId)
+        {
+            Url = url;
+            ClickObjectId = clickObjectId;
+        }
+    }
+}
