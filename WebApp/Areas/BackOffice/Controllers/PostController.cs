91b48711a74960988c0498fb85b082afe842e8b6
Vlad <vlad.petrov.phd@gmail.com>
2019-10-22 14:19:27 +0200

    post

---
+++ WebApp/Areas/BackOffice/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Infrastructure;
+using Common.StringConstants;
+using Domain.Post;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Controllers;
+using WebApp.Model.GenericMvc;
+
+namespace WebApp.Areas.BackOffice.Controllers
+{
+    [Area(AreasNames.BackOfficeArea)]
+    [SetAvailableTableActions(TableActions.Add, TableActions.Details, TableActions.Edit, TableActions.Delete)]
+    public class PostController : MasterPageCrudController<PostDisplay, PostDomain, string>
+    {
+        public PostController(IPostService service) : base(service)
+        {
+        }
+
+        protected override string Title => "Post";
+    }
+}
---
+++ WebApp/Areas/BackOffice/Views/Post/ClientFirmFormDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Domain.Post;
+using WebApp.Model.Forms;
+
+namespace WebApp.Areas.BackOffice.Views.Post
+{
+    public static class PostFormDescriptor
+    {
+        public static FormDescriptor Get(bool readOnly)
+        {
+            throw new NotImplementedException();
+            //return FormBuilder<PostDomain>.CreateNew(readOnly, null)
+            //    .AddItem(x => x.DeliveredDate)
+            //    .AddItem(x => x.Sender)
+            //    .AddItem(x => x.Recipient)
+            //    .AddItem(x => x.Type)
+            //    .AddItem(x => x.Status)
+            //    .AddItem(x => x.RegisteredBy)
+            //    .Build();
+        }
+    }
+}
