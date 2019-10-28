118037fcd297779344c7e31de8ddda3b0b02ee46
Vlad <vlad.petrov.phd@gmail.com>
2019-10-28 17:41:32 +0100

    Settings controller

---
+++ WebApp/Areas/Client/Controllers/ProfileSettingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Areas.Client.Controllers
+{
+    public class ProfileSettingsController : ClientController
+    {
+        [Route("Settings")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
